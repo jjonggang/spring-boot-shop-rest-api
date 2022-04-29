efdf41276d1629dcd919de3bb025204bc88a5da4
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2022-04-30 02:30:19 +0900

    Items to items

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -25,7 +25,7 @@ public class Category {
     joinColumns = @JoinColumn(name = "category_id"), // 내 쪽에서 들어가는 거
             inverseJoinColumns = @JoinColumn(name = "item_id") // item 쪽에서 들어가는 거
     ) // 중간 테이블 mapping
-    private List<Item> Items = new ArrayList<>();
+    private List<Item> items = new ArrayList<>();
 
 
     // 하나의 카테고리는 어떤 카테고리를 부모로 가질 수 있고.
