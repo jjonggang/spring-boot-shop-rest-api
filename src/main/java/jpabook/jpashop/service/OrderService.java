4cf652ea33a6ea5aa11089c0e7640db6e4f7bb47
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2022-05-01 23:10:43 +0900

    Order Service2

--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -47,9 +47,17 @@ public class OrderService {
     }
 
     //취소
+    @Transactional
+    public void cancelOrder(Long orderId){
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        // 주문 취소
+        order.cancel();
+    }
 
-
-    //검색
-
+//    //검색
+//    public List<Order> findOrders(OrderSearch orderSearch){
+//        return orderRepository.findAll(orderSearch);
+//    }
 
 }
